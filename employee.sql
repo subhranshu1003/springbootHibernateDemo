fbd1c806fa0996100a18176d3d4ad8bbc6183a9b
Subhranshu Pujari <subh.pujari@gmail.com>
2020-06-19 19:04:11 +0530

    Add files via upload
    Sql for creating table on mysql and insert query.

---
+++ employee.sql
@@ -0,0 +1,20 @@
+CREATE DATABASE  IF NOT EXISTS `employee_directory`;
+USE `employee_directory`;
+
+--
+-- Table structure for table `employee`
+--
+
+DROP TABLE IF EXISTS `employee`;
+
+CREATE TABLE `employee` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `first_name` varchar(45) DEFAULT NULL,
+  `last_name` varchar(45) DEFAULT NULL,
+  `email` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=latin1;
+
+
+
+
---
+++ employee_insert_query.sql
@@ -0,0 +1,7 @@
+INSERT INTO `employee` VALUES 
+	(1,'Leslie','Andrews','leslie@positive.com'),
+	(2,'Emma','Baumgarten','emma@positive.com'),
+	(3,'Avani','Gupta','avani@positive.com'),
+	(4,'Yuri','Petrov','yuri@positive.com'),
+	(5,'Juan','Vega','juan@positive.com');
+
