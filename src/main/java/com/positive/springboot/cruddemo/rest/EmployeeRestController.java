89747226d9a8e5c0930a02d870100cdf38dcaa92
Subhranshu Pujari <subh.pujari@gmail.com>
2020-07-02 22:28:03 +0530

    swagger documentation added

--- src/main/java/com/positive/springboot/cruddemo/rest/EmployeeRestController.java
+++ src/main/java/com/positive/springboot/cruddemo/rest/EmployeeRestController.java
@@ -3,6 +3,8 @@ package com.positive.springboot.cruddemo.rest;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -10,9 +12,12 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.positive.springboot.cruddemo.model.Employee;
+import com.positive.springboot.cruddemo.model.StatusCheck;
 import com.positive.springboot.cruddemo.service.EmployeeService;
 
 @RestController
@@ -25,7 +30,15 @@ private EmployeeService employeeService;
 	public EmployeeRestController(EmployeeService theEmployeeService) {
 		employeeService = theEmployeeService;
 	}
+	/*
+	private StatusCheck statusCheck;
 	
+	@GetMapping("/test")
+	public StatusCheck test(@RequestBody StatusCheck scheck) {
+		scheck.setStatus("OK");
+		return scheck;
+	}
+	*/
 	// expose "/employees" and return list of employees
 	@GetMapping("/employees")
 	public List<Employee> findAll() {
