8d8c09383553161c20ee543242378a22054a366c
Subhranshu Pujari <subh.pujari@gmail.com>
2020-07-03 14:33:53 +0530

    removing unnecessary imports

--- src/main/java/com/positive/springboot/cruddemo/rest/EmployeeRestController.java
+++ src/main/java/com/positive/springboot/cruddemo/rest/EmployeeRestController.java
@@ -12,12 +12,10 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseStatus;
+
 import org.springframework.web.bind.annotation.RestController;
 
 import com.positive.springboot.cruddemo.model.Employee;
-import com.positive.springboot.cruddemo.model.StatusCheck;
 import com.positive.springboot.cruddemo.service.EmployeeService;
 
 @RestController
